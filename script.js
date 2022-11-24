e5ae173e0117b9574cbdc64cee0d67dfea1285bd
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2022-11-24 00:25:10 +0000

    Got 1 sqaure working

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+const fun = document.getElementsByClassName('singleSquare');
+
+let arrayLength = fun.length;
+
+console.log(arrayLength)
+
+
+
+const colorIt = function myFunction() {
+for (var i = 0; i < 100; i++) {
+
+        const fun = document.getElementsByClassName('singleSquare')[56];
+        fun.addEventListener("mouseover", function(){
+            fun.classList.add("hoverColor");
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// const all = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const colorIt = function myFunction() {
+//   const fun = document.getElementsByClassName("singleSquare")[+1]; //try 0-100? how to say 0 through 100?
+//   fun.addEventListener("mouseover", function () {
+//     fun.classList.add("hoverColor");
+//   });
+// };
