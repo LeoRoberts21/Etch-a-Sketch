9c248547ab49dee54f5354175fdf1c8d93dadf0a
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2022-11-26 00:20:39 +0000

    New grids

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
 
+//CODE FOR PAINT BRUSH -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// const fun = document.getElementsByClassName("singleSquare");
 
+// let arrayLength = fun.length;
 
-
-
-
-const fun = document.getElementsByClassName('singleSquare');
-
-let arrayLength = fun.length;
-
-console.log(arrayLength)
-
-
-
-const colorIt = function myFunction() {
-for (var i = 0; i < 100; i++) {
-
-        const fun = document.getElementsByClassName('singleSquare')[56];
-        fun.addEventListener("mouseover", function(){
-            fun.classList.add("hoverColor");
-        });
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-// const all = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arrayLength);
 
 // const colorIt = function myFunction() {
-//   const fun = document.getElementsByClassName("singleSquare")[+1]; //try 0-100? how to say 0 through 100?
-//   fun.addEventListener("mouseover", function () {
-//     fun.classList.add("hoverColor");
-//   });
+//   for (var i = 0; i < 100; i++) {
+//     const fun = document.getElementsByClassName("singleSquare")[56];
+//     fun.addEventListener("mouseover", function () {
+//       fun.classList.add("hoverColor");
+//     });
+//   }
 // };
+
+
+
+
+//CODE FOR SQUARE CHANGING -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+const TenTen = document.querySelector("gridContainer10x10");
+
+const TwentyTwenty = document.querySelector("gridContainer20x20");
+
+const ThirtyThirty = document.querySelector("gridContainer30x30");
+
+const btnTen = document.querySelector("btn10");
+
+const btnTwenty = document.querySelector("btn20");
+
+const btnThirty = document.querySelector("btn30");
+
+const openTen = function() {
+    TenTen.classList.remove('hidden');
+    TwentyTwenty.classList.add('hidden');
+    ThirtyThirty.classList.add('hidden');
+};
+
+const openTwenty = function() {
+    TenTen.classList.add('hidden');
+    TwentyTwenty.classList.remove('hidden');
+    ThirtyThirty.classList.add('hidden');
+};
+
+const openThirty = function() {
+    TenTen.classList.add('hidden');
+    TwentyTwenty.classList.add('hidden');
+    ThirtyThirty.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+btnTen.addEventListener('click', openTen);
+
+btnTwenty.addEventListener('click', openTwenty);
+
+btnThirty.addEventListener('click', openThirty);
